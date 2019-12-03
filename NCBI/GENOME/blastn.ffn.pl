807adddcf34b4ac2887d3c61df5b5d39321f42ce
LiuHualin <liaochenlanruo@webmail.hzau.edu.cn>
2019-12-03 15:36:19 +0800

    modified the database name

--- NCBI/GENOME/blastn.ffn.pl
+++ NCBI/GENOME/blastn.ffn.pl
@@ -6,5 +6,5 @@ my @fas=glob("*.fna");
 foreach  (@fas) {
 	$_=~/(\S+)_genomic.fna/;
 	my $str = $1 . ".bln";
-	system("blastn -query $_ -out $str -db Alliens -outfmt 6 -evalue 1e-3 -max_target_seqs 1 -num_threads 6");
+	system("blastn -query $_ -out $str -db alleles -outfmt 6 -evalue 1e-3 -max_target_seqs 1 -num_threads 6");
 }
