da4f7268dd8ace9d6b4b5533e3dbb21d94f15827
LiuHualin <liaochenlanruo@webmail.hzau.edu.cn>
2019-12-03 15:32:43 +0800

    Delete unneeded scripts

--- blast_mlst.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-use strict;
-
-my @fa = glob("*-8.fa");
-foreach  (@fa) {
-	$_=~/(\d*[a-zA-Z]+\d+)\S+/;
-	my $blastn = $1 . ".tpi";
-	system("blastn -query $_ -out $blastn -db tpi.db -outfmt 6 -evalue 0.001 -num_threads 6 -max_target_seqs 1");
-}
